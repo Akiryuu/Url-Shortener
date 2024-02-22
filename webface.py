d794118c380a51d8a68628324756f51a104abf76
Akiryuu <Foundy@post.cz>
2024-02-22 12:58:15 +0100

    22.2.

--- webface.py
+++ webface.py
@@ -23,9 +23,6 @@ app.secret_key = b"totoj e zceLa n@@@hodny retezec nejlep os.urandom(24)"
 app.secret_key = b"x6\x87j@\xd3\x88\x0e8\xe8pM\x13\r\xafa\x8b\xdbp\x8a\x1f\xd41\xb8"
 
 
-slova = ("Super", "Perfekt", "Úža", "Flask")
-
-
 def prihlasit(function):
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
